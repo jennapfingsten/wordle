083ac4c4ff7ab435b86a1878eb6636b2bc19c711
Jenna Pfingsten <jennapfingsten17@gmail.com>
2022-03-22 22:29:27 -0500

    Dynamically adjust height/width of board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import styles from './Board.module.css';
 
 const Board = ({ boardConfig }) => {
+	const keyboardHeight = 200;
+	const bannerHeight = 50;
 	/*
 	Status:
 	- "" = nothing
@@ -10,28 +12,47 @@ const Board = ({ boardConfig }) => {
 	- present = yellow
 	- absent = grey
 	*/
+	const boardEl = useRef(null);
+
+	const onResize = () => {
+		boardEl.current.style.width =
+			((window.innerHeight - keyboardHeight - bannerHeight) * 5) / 6 + 'px';
+	};
+
+	useEffect(() => {
+		if (boardEl && boardEl.current) {
+			onResize();
+			window.addEventListener('resize', onResize);
+		}
+
+		return () => {
+			window.removeEventListener('resize', onResize);
+		};
+	}, []);
 
 	return (
-		<div className={styles.tileContainer}>
-			{boardConfig.map((row, i) => {
-				return (
-					<React.Fragment key={i}>
-						{row.map((tile) => {
-							const divStyle = { backgroundColor: tile.status };
-							return (
-								<div
-									key={tile.id}
-									className={`${styles.tile} ${tile.status}`}
-									style={divStyle}
-									data-status={tile.status}
-								>
-									{tile.letter}
-								</div>
-							);
-						})}
-					</React.Fragment>
-				);
-			})}
+		<div className={styles.board}>
+			<div className={styles.tileContainer} ref={boardEl}>
+				{boardConfig.map((row, i) => {
+					return (
+						<React.Fragment key={i}>
+							{row.map((tile) => {
+								const divStyle = { backgroundColor: tile.status };
+								return (
+									<div
+										key={tile.id}
+										className={`${styles.tile} ${tile.status}`}
+										style={divStyle}
+										data-status={tile.status}
+									>
+										{tile.letter}
+									</div>
+								);
+							})}
+						</React.Fragment>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
